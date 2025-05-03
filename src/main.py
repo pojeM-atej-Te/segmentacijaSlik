7c53ec85dfd161b43187336ff3afcb5209ae606e
Matej Poje <matej.poje@student.um.si>
2025-05-03 11:34:54 +0200

    SEG-3: Implementacija centrov

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def kmeans(slika, k=3, iteracije=10):
     '''Izvede segmentacijo slike z uporabo metode k-means.'''
     h, w, c = slika.shape
@@ -59,9 +58,87 @@ def meanshift(slika, velikost_okna, dimenzija):
     '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
     pass
 
+
 def izracunaj_centre(slika, izbira, dimenzija_centra, T):
     '''Izračuna centre za metodo kmeans.'''
-    pass
+    h, w, c = slika.shape
+    k = 3  # Privzeto število centrov
+
+    if izbira == "ročno":
+        # Implementacija ročne izbire centrov
+        def on_click(event, x, y, flags, param):
+            if event == cv.EVENT_LBUTTONDOWN:
+                centri.append(np.array(slika[y, x]))
+                print(f"Izbrana barva: {slika[y, x]}")
+
+        centri = []
+        okno = "Izberi centre"
+        cv.namedWindow(okno)
+        cv.setMouseCallback(okno, on_click)
+
+        cv.imshow(okno, cv.cvtColor(slika, cv.COLOR_BGR2RGB))
+
+        while len(centri) < k:
+            key = cv.waitKey(1) & 0xFF
+            if key == 27:  # ESC za izhod
+                break
+
+        cv.destroyAllWindows()
+        centri = np.array(centri)
+
+    else:  # naključna izbira
+        if dimenzija_centra == 3:  # samo barve
+            centri = []
+            while len(centri) < k:
+                # Naključno izberi piksel
+                y = np.random.randint(0, h)
+                x = np.random.randint(0, w)
+                novi_center = slika[y, x].astype(np.float64)
+
+                # Preveri razdaljo do obstoječih centrov
+                if not centri:
+                    centri.append(novi_center)
+                else:
+                    dodaj = True
+                    for center in centri:
+                        if manhattanska_razdalja(center, novi_center) < T:
+                            dodaj = False
+                            break
+                    if dodaj:
+                        centri.append(novi_center)
+
+            centri = np.array(centri)
+
+        elif dimenzija_centra == 5:  # barve + lokacije
+            centri = []
+            while len(centri) < k:
+                # Naključno izberi piksel
+                y = np.random.randint(0, h)
+                x = np.random.randint(0, w)
+                barva = slika[y, x].astype(np.float64)
+
+                # Normaliziraj koordinate na podoben razpon kot barve
+                y_norm = y * 255.0 / h
+                x_norm = x * 255.0 / w
+
+                novi_center = np.concatenate(([x_norm, y_norm], barva))
+
+                # Preveri razdaljo do obstoječih centrov
+                if not centri:
+                    centri.append(novi_center)
+                else:
+                    dodaj = True
+                    for center in centri:
+                        if manhattanska_razdalja(center, novi_center) < T:
+                            dodaj = False
+                            break
+                    if dodaj:
+                        centri.append(novi_center)
+
+            centri = np.array(centri)
+
+    return centri
+
 
 def manhattanska_razdalja(vektor1, vektor2):
     """Izračuna manhattansko razdaljo med dvema vektorjema."""
